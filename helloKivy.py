89a6a8df9060202897d58b6458e44462f3317ebb
Aakash Karthikeyan <aakashkarthikeyan3@gmail.com>
2023-12-04 16:10:35 +0530

    commitErr

--- helloKivy.py
+++
@@ -1,9 +0,0 @@
-from kivy.app import App
-from kivy.uix.label import Label
-
-class HelloKivy(App):
-    def build(self):
-        return Label(text="Hello Kivy!!")
-
-if __name__ == "__main__":
-    HelloKivy().run()
